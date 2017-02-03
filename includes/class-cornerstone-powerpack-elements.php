2b9486e381f3e419cc0ea2fdca886ad32b42e95c
Irvin Lee Eduliantes <irvin@d3fy.com>
2017-02-03 18:23:10 +0800

    fixed the bug that caused the elements to not be dispayed on the admin dashboard

--- includes/class-cornerstone-powerpack-elements.php
+++ includes/class-cornerstone-powerpack-elements.php
@@ -8,30 +8,32 @@
  */
 
 class Cornerstone_Powerpack_Elements {
-  
+
   protected static $elements = array();
   protected static $instance = null;
-  
+
   // private singleton constructor (use getInstance method instead)
 	private function __construct() {
         self::$elements = array();
 	}
-	
+
 	// get static instance of class object
 	public static function &getInstance() {
 		if (!(self::$instance instanceof self)) self::$instance = new self();
 		return self::$instance;
 	}
-	
+
 	// register element
 	public function register($key, $name, $desc='', $opts=array()) {
-        self:$elements[$key] = array(
+    $instance = self::getInstance();
+
+        $instance::$elements[$key] = array(
             'name'  => $name,
             'desc'  => $desc,
             'opts'  => $opts,
         );
 	}
-	
+
 	// Set element option
 	public function set_option($element_key, $opt_key, $opt_val) {
   	if (isset($this->elements[$element_key])) {
@@ -39,19 +41,19 @@ class Cornerstone_Powerpack_Elements {
     	return true;
     } else return false;
 	}
-	
+
 	// Get list of all available elements.
 	public static function get_elements() {
   	$self = self::getInstance();
   	return ($self::$elements);
 	}
-	
+
 	// Get list of all available element keys.
 	public static function get_element_keys() {
   	$elements = self::get_elements();
 		return array_keys($elements);
 	}
-	
+
 	// Get element options object by element key.
 	public static function get_element_opts($key) {
   	$self = self::getInstance();
