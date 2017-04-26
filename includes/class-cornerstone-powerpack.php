3e7405382dad9b47601c1accabf796606f0e481c
Nate Baldwin <nbaldwin@mindpalette.com>
2017-04-26 12:03:54 -0400

    increment version number to 0.1.10 for update release

--- includes/class-cornerstone-powerpack.php
+++ includes/class-cornerstone-powerpack.php
@@ -28,7 +28,7 @@ class Cornerstone_Powerpack {
 	public function __construct() {
 		
 		$this->cornerstone_powerpack = 'cornerstone-powerpack';
-		$this->version = '0.1.9';
+		$this->version = '0.1.10';
 		// $this->version = time();
 		$this->dashboardoptskey = $this->cornerstone_powerpack.'-settings-dashboard';
 		
