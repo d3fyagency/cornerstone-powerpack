3604a0a1c4b1fbc13a59c0e06fdb79c37d2f143c
Irvin Lee Eduliantes <irvin@d3fy.com>
2017-05-29 21:01:43 +0800

    details slide-up

--- elements/team-members/includes/csl-team-member/layouts/details-slideup.php
+++ elements/team-members/includes/csl-team-member/layouts/details-slideup.php
@@ -1,11 +1,23 @@
-<div <?php echo cs_atts( array( 'id' => $id, 'class' => $class, 'style' => $style ) ); ?>>
+<?php
+$social = array();
+foreach ($social_networks as $key=>$info) {
+  $social_var = 'social_'.$key;
+  if ($$social_var) {
+    $social[] =
+      '<a href="'.esc_html($$social_var).'" target="_blank" class="social-link social-link-'.esc_attr($key).'">'
+      .'<i class="social-icon '.esc_attr($info['icon']).'"></i>'
+      .'</a>';
+  }
+}
+?>
+
+<div <?php echo cs_atts( array( 'id' => $id, 'class' => $class . ' details-slideup', 'style' => $style ) ); ?>>
   <div class="teammember-image-wrap">
     <img src="<?= $member_image ?>" />
     <div class="teammember-info" style="background-color:<?= $highlight_color ?>;">
       <div class="teammember-name"><?= $name ?></div>
       <div class="teammember-title"><?= $job_title ?></div>
     </div>
-    <div class="view-profile" style="background-color:<?= $highlight_color ?>;"><?= $custom_open_text ?></div>
   </div>
   <div class="teammember-bio-overlay">
     <div class="teammember-bio-container">
@@ -17,6 +29,15 @@
         <?php echo do_shortcode( $content ); ?>
         <?= $member_content ?>
       </div>
+      <?php if (!empty($social)): ?>
+      <div class="teammember-social"><ul>
+        <?php foreach ($social as $s_link): ?>
+        <li style="font-size:<?=$social_logo_font_size?>; color:<?= $social_logo_color ?>">
+          <?= $s_link; ?>
+        </li>
+        <?php endforeach; ?>
+      </ul></div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
