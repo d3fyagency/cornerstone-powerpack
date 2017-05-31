501508c67c7f55ad68fb1ae0835ecd4cf38127c4
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2017-05-31 22:57:48 +0800

    Added pie and chart graph.

--- elements/charts/includes/cs-charts/controls.php
+++ elements/charts/includes/cs-charts/controls.php
@@ -10,9 +10,11 @@ return array(
 		'ui'      => array(
 			'title'   => __( 'Chart Style', 'cs-charts' ),
 		),
+		'context' => 'content',
 		'options' => array(
 			'choices' => array(
 				array( 'value' => 'pie',   'label' => __( 'Pie', 'cs-charts' ) ),
+				array( 'value' => 'donut',   'label' => __( 'Donut', 'cs-charts' ) ),
 				array( 'value' => 'bar',  'label' => __( 'Bar', 'cs-charts' ) ),
 			)
 		)
@@ -26,7 +28,6 @@ return array(
 			'capacity' => 5,
 			'title_field' => 'heading'
 		),
-		'context' => 'content',
 		'suggest' => array(
 			array( 'heading' => __( 'Item 1', 'cs-charts' ) ),
 			array( 'heading' => __( 'Item 2', 'cs-charts' ) ),
