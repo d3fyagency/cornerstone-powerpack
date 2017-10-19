cbabc3870b869f30559b21628ee2f397c9610054
Nate Baldwin <nbaldwin@mindpalette.com>
2017-10-18 21:29:05 -0400

    switch to inserted modal content to fix container bug

--- elements/team-members/team-members.php
+++ elements/team-members/team-members.php
@@ -36,11 +36,11 @@ add_action( 'wp_enqueue_scripts', 'cspp_teammember_scripts');
 
 if (!function_exists('cspp_teammember_scripts')) {
 	function cspp_teammember_scripts() {
-  	$v = 20170515;
+  	$v = 20171018;
   	// $v = time();
-
+    wp_enqueue_script( 'cspp-lity', D3FY_CSPP_URL.'/lib/cspplity/lity.min.js', array( 'jquery' ), $v );  	
 	  wp_enqueue_script( 'csl-teammember-script', CSL_TEAMMEMBER_URL . 'assets/js/csl_teammember.js?' . time(), array('jquery'), $v, true );
-	  wp_enqueue_style( 'csl-teammember-style', CSL_TEAMMEMBER_URL . 'assets/css/csl_teammember.css?' . time(), array(), $v );
+	  wp_enqueue_style( 'csl-teammember-style', CSL_TEAMMEMBER_URL . 'assets/css/teammember.css?' . time(), array(), $v );
 	  wp_enqueue_style( 'cspp-icons', D3FY_CSPP_URL.'/lib/csppicons/style.css', array(), $v, 'all' );
 	}
 }
