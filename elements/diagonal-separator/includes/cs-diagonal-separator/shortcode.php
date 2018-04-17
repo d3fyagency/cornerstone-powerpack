a98d418ccbc37e69e47dd8308ba36311884f2b6b
Nate Baldwin <nbaldwin@mindpalette.com>
2018-04-17 14:52:05 -0400

    fix css bug for non-overlapping diagonals

--- elements/diagonal-separator/includes/cs-diagonal-separator/shortcode.php
+++ elements/diagonal-separator/includes/cs-diagonal-separator/shortcode.php
@@ -19,27 +19,23 @@ $classes = array('cs-diagonal-separator', 'cs-diagonal-separator-'.$count);
 if ($class) $classes[] = $class;
 $attr_container = cs_atts(array(
   'id'    => $id,
-	'class'	=> esc_attr(implode(' ', $classes)),
-	'style' => $style,
+  'class' => esc_attr(implode(' ', $classes)),
+  'style' => $style,
 ));
 
 // generate SVG
 if ($valign == 'bottom') {
-  if ($slope == 'point') $points = '0 100, 50 0, 100 100';
-  else if ($slope == 'dip') $points = '0 0, 50 100, 100 0, 100 100, 0 100';
-	else if ($slope == 'down') $points = '0 0, 100 100, 0 100';
-	else $points = '0 100, 100 0, 100 100';
+  if ($slope == 'point') $shape = '<polygon points="0 100, 50 0, 100 100" />';
+  else if ($slope == 'dip') $shape = '<polygon points="0 0, 0 100, 50 100" /><polygon points="100 0, 100 100, 50 100" />';
+  else if ($slope == 'down') $shape = '<polygon points="0 0, 100 100, 0 100" />';
+  else $shape = '<polygon points="0 100, 100 0, 100 100" />';
 } else {
-  if ($slope == 'point') $points = '0 0, 50 100, 100 0';
-  else if ($slope == 'dip') $points = '0 0, 0 100, 50 0, 100 100, 100 0';
-	else if ($slope == 'down') $points = '0 0, 100 0, 100 100';
-	else $points = '0 0, 100 0, 0 100';
+  if ($slope == 'point') $shape = '<polygon points="0 0, 50 100, 100 0" />';
+  else if ($slope == 'dip') $shape = '<polygon points="0 100, 0 0, 50 0" /><polygon points="50 0, 100 0, 100 100" />';
+  else if ($slope == 'down') $shape = '<polygon points="0 0, 100 0, 100 100" />';
+  else $shape = '<polygon points="0 0, 100 0, 0 100" />';
 }
-
-$svg =
-'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">'.
-  '<polygon fill="'.esc_attr($color).'" points="'.$points.'"/>'.
-'</svg>';
+$svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none" style="fill: '.esc_attr($color).';">'.$shape.'</svg>';
 
 // set wrapper CSS
 $wrapHeight = ($overlap == 'none') ? intval($height) : 0;
@@ -49,18 +45,19 @@ $ElementAdmin->addCSSRule(
 );
 
 // set shape CSS
-$vposition = ($overlap == 'above') ? 'bottom: 0px;' : 'top: 0px;';
+$vposition = ($overlap == 'above') 
+  ? 'top: 0; transform: translate(0, -'.intval($height).'px);' 
+  : 'bottom: 0; transform: translate(0, '.intval($height).'px);';
+/* $vposition = ($overlap == 'above') 
+  ? 'top: -'.intval($height).'px;' 
+  : 'bottom: -'.intval($height).'px;'; */
 $ElementAdmin->addCSSRule( 
   trim($css_prefix.' .shape'),
-  'position: absolute; left: 0px; '.$vposition.' width: 100%; height: '.intval($height).'px; z-index: '.intval($zindex).';'
+  'position: absolute; margin: 0 auto; left: 0; right: 0; '.$vposition.' width: 100%; height: '.intval($height).'px; z-index: '.intval($zindex).';'
 );
 $ElementAdmin->addCSSRule( 
-  trim($css_prefix.' .shape'),
-  'background-image: url(\'data:image/svg+xml,'.rawurlencode($svg).'\')'
-);
-$ElementAdmin->addCSSRule( 
-  trim($css_prefix.' .shape'),
-  'background-size: 100% 100%; background-repeat: no-repeat;'
+  trim($css_prefix.' .shape svg'),
+  'width: 100%; height: 100%;'
 );
 
 ob_start();
@@ -68,7 +65,7 @@ ob_start();
 ?>
 
 <div <?php echo $attr_container; ?> >
-	<div class="shape"></div>
+  <div class="shape"><?php echo $svg; ?></div>
 </div>
 
 <?php
